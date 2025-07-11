c23daaa9896257f68f959fe82946b1d12041f8ab
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-07-11 12:59:53 +0600

    init

---
+++ SeleniumTraining/src/test/java/ui/ResizeWidget.java
@@ -0,0 +1,58 @@
+package ui;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
+import java.time.Duration;
+
+import java.util.*;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ResizeWidget {
+
+	public static String browser = "chrome"; // External Configuration - XLS,CSV
+	public static WebDriver driver;
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		if (browser.equals("chrome")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+
+		} else if (browser.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+
+		}
+
+		driver.get("https://jqueryui.com/resizable/");
+		driver.manage().window().maximize();
+
+		WebElement frame = driver.findElement(By.xpath("//*[@id=\"content\"]/iframe"));
+		driver.switchTo().frame(frame);
+
+		WebElement element = driver.findElement(By.xpath("//*[@id=\"resizable\"]/div[3]"));
+
+		Actions action = new Actions(driver);
+		action.dragAndDropBy(element, 300, 100).perform();
+		Thread.sleep(2000);
+
+	}
+
+}
---
+++ SeleniumTraining/src/test/java/ui/Slider.java
@@ -0,0 +1,58 @@
+package ui;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
+import java.time.Duration;
+
+import java.util.*;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Slider {
+
+	public static String browser = "chrome"; // External Configuration - XLS,CSV
+	public static WebDriver driver;
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		if (browser.equals("chrome")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+
+		} else if (browser.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+
+		}
+
+		driver.get("https://jqueryui.com/slider/#colorpicker");
+		driver.manage().window().maximize();
+
+		WebElement frame = driver.findElement(By.xpath("//*[@id=\"content\"]/iframe"));
+		driver.switchTo().frame(frame);
+
+		WebElement sliderGreen = driver.findElement(By.xpath("//*[@id=\"green\"]"));
+
+		Actions action = new Actions(driver);
+		action.dragAndDropBy(sliderGreen, -106, 125).perform();
+		Thread.sleep(2000);
+
+	}
+
+}
