3317f7777f00a1d2c4fd5a5ebaa12120dcf1b3c3
Tanvir Hassan <101284808+RogerThatTan@users.noreply.github.com>
2025-07-12 00:57:49 +0600

    TestNG

--- Sel-TestNG/src/test/java/ui/LoginTest.java
+++ Sel-TestNG/src/test/java/ui/LoginTest.java
@@ -1,10 +1,45 @@
 package ui;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class LoginTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@BeforeTest
+	public void loginToApplication() {
+		System.out.println("Login to Application");
 
+	}
+
+	@AfterTest
+	public void logoutFromApplication() {
+		System.out.println("Logout From Application");
+
+	}
+
+	@BeforeMethod
+	public void connectToDB() {
+		System.out.println("BM DB Connected");
+
+	}
+
+	@AfterMethod
+	public void disconnectFromDB() {
+		System.out.println("AM Disconnect DB cONNECTION");
+
+	}
+
+	@Test(priority = 1, description = "This is a login test!")
+	public void loginTest() {
+		System.out.println("Login is sucessful");
+	}
+
+	@Test(priority = 2, description = "This is a logout test!")
+	public void logoutTest() {
+		System.out.println("Log Out is sucessful");
 	}
 
 }
